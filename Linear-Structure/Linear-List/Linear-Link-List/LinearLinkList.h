17bc56091e71ecd51bc98fe6d4b40f2b7d9cc4ba
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2021-03-14 20:57:32 +0800

    Update LinearLinkList-add basic struct and init empty

---
+++ Linear-Structure/Linear-List/Linear-Link-List/LinearLinkList.h
@@ -0,0 +1,15 @@
+#define OK 1
+    //If a funtion functions well, it would return OK.
+#define ERROR 0
+    //If a funtion has something wrong, it would return ERROR.
+#define OVERFLOW -1
+
+typedef int Status;
+typedef int Elemtype;
+typedef struct LNode
+{
+    Elemtype data;
+    LNode *next;
+}LNode, *LinkList;
+    //A LNode includes data and a pointer, which points to the next node.
+    //Type LinkList is a pointer of LNode, and it is also called a head pointer.
---
+++ Linear-Structure/Linear-List/Linear-Link-List/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"LinearLinkList.h"
+
+int main()
+{
+    LinkList L;
+    L = NULL;
+        //L = NULL means that L is empty.
+
+        return 0;
+}
