556741317467d3d9132623781efe4606db2895db
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2021-04-22 22:40:19 +0800

    Update Array: modify Array.h, add basic defination of Array

--- Non-Linear-Structure/Array/Array.h
+++ Non-Linear-Structure/Array/Array.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define OK 1
+#define ERROR 0
+
 typedef int Status;
+typedef int ElemType;
+typedef struct
+{
+        // base location of the array
+    ElemType *base;
+        // dimensions of the array
+    int dim;
+        // the size of each dimension b_i
+    int *bounds;
+        // the constant base location c_i of array mapping function
+    int *constants;
+}Array;
 
 #endif
