2d6d1fd95bfd7e02d194a345c8d4cd44f1d2efbe
MiaoHoa-oops <haomiao19@mails.ucas.ac.cn>
2021-05-03 08:17:44 +0800

    Update BLT: add BT.*, add declaretion of the function CreateBiTree_Pre

---
+++ Tree-Structure/Binary-Tree/Binary-Linked-Tree/BT.h
@@ -0,0 +1,12 @@
+#ifndef _BT_H_
+#define _BT_H_
+
+#include "main.h"
+
+Status CreateBiTree_Pre(BTree *B);
+    /*
+     * CreateBiTree_pre functions as follows:
+     * create a binary tree by input the preorder traversal result
+     */
+
+#endif
